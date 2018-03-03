fb8648e838f4a34ee18c5bdbc3c9c99a8cc59e9b
unknown <misha.baturin96@gmail.com>
2018-03-03 19:21:35 +0200

    add local storage

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,16 @@
 import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import reducer from "./reducers";
+import { loadState, saveState } from "./local-storage/localStorage";
+
+const persistedstate = loadState();
 
 const middleware = applyMiddleware(logger);
-export const store = createStore(reducer, middleware);
+export const store = createStore(reducer, persistedstate);
+
+store.subscribe(() => {
+  saveState(store.getState());
+});
+
 export * from "./actions";
 export * from "./reducers";
